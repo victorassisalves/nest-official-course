9b799d49abbdfe6d789aec03c69f996adf23c594
Victor Assis <victorassis@Victors-MacBook-Air.local>
2022-09-16 11:32:47 -0300

    Added the @Entity to our coffee entity and imported to the coffee module.

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { CoffeeEntity } from './entities/coffee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CoffeeEntity])], // forFeature() to register entities
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('coffee')
 export class CoffeeEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
